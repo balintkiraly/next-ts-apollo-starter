be1f2a2a2690235139fe247ecb2475598f07db77
balintkiraly <balint.kiraly@protonmail.com>
2020-11-01 19:50:41 +0100

    Add apollo with an example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,41 @@
-import { Box } from "@chakra-ui/core"
+
+import React from 'react'
+import Head from 'next/head'
+import { useQuery } from '@apollo/client'
+import { Box, Image, Grid, Heading } from '@chakra-ui/core'
+
+import QUERY from './exampleQuery.graphql'
 
 export default function Home() {
+  const { data, loading, error } = useQuery(QUERY)
+  
+  if (loading) {
+    return <Heading>Loading...</Heading>
+  }
+
+  if (error) {
+    return <Heading>Something went wrong</Heading>
+  }
+
   return (
-    <Box>
-      Welcome to <a href="https://nextjs.org">Next.js!</a>
-    </Box>
+    <>
+      <Head>
+        <title>GraphQL Example</title>
+      </Head>
+      <Box m={5}>
+        <Heading my={4}>Rick and Morty characters</Heading>
+        <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+          {data.characters.results.map((character) => (
+            <Box  maxW="sm" borderWidth="1px" rounded="lg"  key={character.id}>
+              <Image src={character.image} />
+
+              <Box textAlign="center" fontWeight="bold" p={3}>
+                {character.name}
+              </Box>
+            </Box>
+          ))}
+        </Grid>
+      </Box>
+    </>
   )
 }
